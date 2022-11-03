9628b16fed111a61a929842f918df3d5ffa90cc3
modengann <99417989+modengann@users.noreply.github.com>
2022-11-03 10:23:32 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,35 @@
+/** YouTube part 1: https://youtu.be/X9xbyRV9F7Q
+ *  YouTube part 2: https://youtu.be/hg_M2rFnYSQ
+ *  Goals:
+ *  1. Add an animation loop
+ *  2. Add "rain" ovals to the ArrayList rain based on mouse clicks
+ *  3. Complete the rainFall method so that all the stored rain will fall towards the bottom of the screen
+ */
+
+import javadraw.*;
+import java.util.*;
+
+public class App extends Window{
+    ArrayList<Oval> rain;
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+    //Task 1: add the animation loop, with a call to rainFall()
+    public void start(){
+
+
+    }
+    //Task 2: Add "rain" to the provided ArrayList based on the location clicked
+    public void mouseDown(Location loc, int button){
+        
+    }
+
+    //Task 3: Complete this code so that it will cause all the objects in rain to "fall"
+    public void rainFall(){
+
+    }
+
+}
